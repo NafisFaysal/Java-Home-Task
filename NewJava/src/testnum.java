5b7320cb7bc0a9820822644cdac54b7465958569
NafisFaysal <nafisfaysal@gmail.com>
2016-11-25 12:11:22 +0600

    Lab Task day 2

---
+++ NewJava/src/testnum.java
@@ -0,0 +1,55 @@
+/**
+ * Created by nafis on 11/3/16.
+ *//*
+
+public class testnum {
+    public static void main(String [] args){
+        int values[] = new int[130];
+        for (int i =0;i<values.length;i++){
+            System.out.println(values[i]);
+
+        }
+        //computeSum()
+
+
+    }
+
+    public boolean searchHEads(boolean [] heads){
+        int streak =0;
+
+        for (int i =0;i<heads.length;i++) {
+            if (heads[i]) {
+                streak++;
+                if(streak ==10){
+                    return true;
+                }            }
+        }
+
+
+                else{
+            streak = 0;
+
+        }
+    }
+
+    int findSamllest(int [] values){
+        int minIndex =0;
+        for (int i =1;i<values.length;i++){
+            if(values[i] <values[minIndex]){
+                minIndex =i;
+            }
+        }
+        return minIndex;
+    }
+
+    public int computeSum(int [] nums){
+    int sum = 0;
+    for (int i =0;i<nums.length;i++){
+        sum = sum+nums[i];
+    }
+    return sum;
+}
+
+
+}
+*/
